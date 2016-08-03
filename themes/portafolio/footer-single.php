e7a1558253eb614bb52f8b3c7c1ac65bd42d3cd3
oscar <oscaroceguera@gmail.com>
2016-08-03 02:37:01 -0600

    single

---
+++ themes/portafolio/footer-single.php
@@ -0,0 +1,16 @@
+<footer>
+  <div class="max-width">
+    <div class="block max-width">
+      <div class="block__title">
+        Footer
+      </div>
+      <div class="block__body">
+        <small>Esta fue una entrada del portafolio de Oscar</small>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ themes/portafolio/single.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+  <body>
+    <?php
+      if(have_posts()) :
+        while(have_posts()) :
+          the_post();
+    ?>
+        <h1 class="page-heading max-width"><?php the_title(); ?></h1>
+        <main class="max-width">
+          <article class="block">
+            <div class="block__title">Escrito por: <?php the_author(); ?></div>
+            <div class="block__body">
+              <p>
+                <?php the_content(); ?>
+              </p>
+              <footer>
+                <div>
+                  <small><?php the_tags(); ?></small>
+                </div>
+              </footer>
+            </article>
+        </main>
+    <?php
+      endwhile;
+      else :
+    ?>
+      <h4>Huy, no encontramos posts</h4>
+    <?php
+      endif;
+      wp_reset_postdata();
+    ?>
+  </p>
+
+<?php get_footer("single"); ?>

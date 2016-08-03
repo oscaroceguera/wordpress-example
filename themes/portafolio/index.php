6a475d2b7115e865afe2205fbd28640e849f3ce6
oscar <oscaroceguera@gmail.com>
2016-08-03 02:20:05 -0600

    the _loop y sus filtros

--- themes/portafolio/index.php
+++ themes/portafolio/index.php
@@ -6,13 +6,16 @@
         <div class="block__title">
           Últimos Trabajos
         </div>
-        <div class="bock__body grid">
+        <div class="block__body grid">
           <p>
             <?php
-              if (have_posts()) {
-                while (have_posts()) {
-                  the_post();
-                  ?>
+              $args = array('cat' => 8, 'author' => 'oceguera', 'posts_per_page' => 1);
+              $filter_posts = new WP_Query($args);
+
+              if($filter_posts->have_posts()) :
+                while($filter_posts->have_posts()) :
+                  $filter_posts->the_post();
+            ?>
                     <article class="block grid--item-4">
                       <h2 class="block__title"><?php the_title(); ?></h2>
                       <div class="block__body">
@@ -28,9 +31,14 @@
                         </div>
                       </footer>
                     </article>
-                  <?php
-                }
-              }
+            <?php
+              endwhile;
+              else :
+            ?>
+              <h4>Huy, no encontramos posts</h4>
+            <?php
+              endif;
+              wp_reset_postdata();
             ?>
           </p>
         </div>
